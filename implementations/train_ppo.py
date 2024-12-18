6ae199fcfde79b201245b2dff36022d48ce58bb1
jknyspel-catalogicsoftware <107405046+jknyspel-catalogicsoftware@users.noreply.github.com>
2024-12-18 14:03:10 +0100

    Save/load agent in `train_ppo()` + clean up imports

--- implementations/train_ppo.py
+++ implementations/train_ppo.py
@@ -1,36 +1,35 @@
-import torch
-from torch import optim
-import numpy as np
-from torch import Tensor
 from pettingzoo import ParallelEnv
-from dataclasses import dataclass
-from nmmo.entity.entity import EntityState
 
 from implementations.PpoAgent import PPOAgent
-from implementations.Observations import Observations, ActionTargets, EntityData, to_observations
+from implementations.Observations import to_observations
 
 
 def train_ppo(
     env: ParallelEnv,
+    *,
     episodes: int = 1000,
-    print_every: int = 1
+    print_every: int | None = 1,
+    save_every: int | None = 100,
+    agent_name: str | None = None,
+    start_state_name: str | None = None
 ) -> None:
-    agent = PPOAgent()
+    agent_name = agent_name or 'ppo_agent'
+    agent = PPOAgent.load(start_state_name) if start_state_name is not None else PPOAgent()
     avg_rewards = []
-    
+
     for episode in range(1, episodes+1):
         states, _ = env.reset()
         episode_data = {
             agent_id: {
-                'states': [], 
-                'actions': [], 
+                'states': [],
+                'actions': [],
                 'rewards': [],
-                'log_probs': [], 
+                'log_probs': [],
                 'dones': []
             }
             for agent_id in env.agents
         }
-        
+
         total_rewards: dict[int, float] = {agent_id: 0.0 for agent_id in env.agents}
         while env.agents:  # While there are active agents
             observations = {
@@ -44,9 +43,9 @@ def train_ppo(
                 agent_id: action_data[agent_id][0]
                 for agent_id in env.agents
             }
-            
+
             next_states, rewards, terminations, truncations, _ = env.step(env_actions)
-            
+
             for agent_id in env.agents:
                 episode_data[agent_id]['states'].append(observations[agent_id])
                 episode_data[agent_id]['actions'].append(action_data[agent_id][1])
@@ -56,9 +55,9 @@ def train_ppo(
                     terminations[agent_id] or truncations[agent_id]
                 )
                 total_rewards[agent_id] += rewards[agent_id]
-            
+
             states = next_states
-        
+
         agent.update(
             {aid: data['states'] for aid, data in episode_data.items()},
             {aid: data['actions'] for aid, data in episode_data.items()},
@@ -69,6 +68,9 @@ def train_ppo(
 
         avg_reward = sum(total_rewards.values()) / len(total_rewards)
         avg_rewards.append(avg_reward)
-        
-        if episode % print_every == 0:
+
+        if print_every is not None and (episode % print_every == 0 or episode == episodes):
             print(f"Episode {episode}, Average Reward: {avg_reward:.2f}")
+
+        if save_every is not None and (episode % save_every == 0 or episode == episodes):
+            agent.save(f"{agent_name}_at_ep{episode}")
